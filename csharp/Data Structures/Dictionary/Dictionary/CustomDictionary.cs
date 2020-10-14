4afb19dd4ea93659f4f4e85ea08c68c4c40356c8
Rodion <rodion.lim@hotmail.com>
2020-10-14 09:14:05 +0800

    Added dictionary data structure implementation

---
+++ csharp/Data Structures/Dictionary/Dictionary/CustomDictionary.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class CustomDictionary
+    {
+        // Hash Table : Chaining
+        LinkedList<KeyValuePair>[] items;
+        readonly int size;
+        private class KeyValuePair
+        {
+            public int k;
+            public string v;
+            public KeyValuePair(int key, string val)
+            {
+                k = key;
+                v = val;
+            }
+        }
+
+        public CustomDictionary(int s)
+        {
+            items = new LinkedList<KeyValuePair>[s];
+            size = s;
+        }
+
+        public void Put(int k, string val)
+        {
+            var entry = GetEntry(k);
+            if (entry != null)
+            {
+                entry.v = val;
+                return;
+            }
+
+            GetOrCreateBucket(k).AddLast(new KeyValuePair(k, val));
+        }
+
+        public string Get(int k)
+        {
+            var entry = GetEntry(k);
+            return (entry != null) ? entry.v : string.Empty;
+        }
+
+        public void Remove(int k)
+        {
+            var entry = GetEntry(k);
+            if (entry != null) GetBucket(k).Remove(entry);
+        }
+
+        private KeyValuePair GetEntry(int k)
+        {
+            var bucket = GetBucket(k);
+            if (bucket != null)
+            {
+                foreach (var node in bucket)
+                    if (node.k == k)
+                        return node;
+            }
+            return null;
+        }
+
+        private int GetHashIndex(int k)
+        {
+            return k % size;
+        }
+
+        private LinkedList<KeyValuePair> GetBucket(int k)
+        {
+            return items[GetHashIndex(k)];
+        }
+
+        private LinkedList<KeyValuePair> GetOrCreateBucket(int k)
+        {
+            var index = GetHashIndex(k);
+            if (items[index] == null)
+                items[index] = new LinkedList<KeyValuePair>();
+
+            return items[index];
+        }
+    }
+}
---
+++ csharp/Data Structures/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindFirstNonRepeatCharacter("A Green Apple"));
+            Console.WriteLine(FirstRepeatedCharacter("green apple"));
+
+            var dict = new CustomDictionary(10);
+            dict.Put(15, "test 1");
+            dict.Put(10, "hello");
+            dict.Put(5, "test 2");
+            dict.Put(5, "Expected string");
+
+            Console.WriteLine(dict.Get(5));
+
+            dict.Remove(10);
+            Console.WriteLine(dict.Get(10));
+        }
+
+        private static char FirstRepeatedCharacter(string input)
+        {
+            // Here, a set is more relevant as we do not care about the number of times the char is repeated
+            var set = new HashSet<char>();
+            foreach (var c in input)
+            {
+                if (set.Contains(c))
+                    return c;
+                set.Add(c);
+            }
+            return Char.MinValue;
+        }
+
+        private static char FindFirstNonRepeatCharacter(string input)
+        {
+            // Here, we use a dictionary to store non repeated characters
+            // Then, we iterate the input value to check if they exist in the non repeated dictionary
+            var noRepeatDict = new Dictionary<char,bool>();
+            var repeatDict = new Dictionary<char, bool>();
+            input = input.ToLower();
+
+            if (input.Length == 0) throw new System.InvalidOperationException("Empty string not accepted");
+
+            foreach (var c in input)
+            {
+                if (c == ' ') continue;
+                if (noRepeatDict.ContainsKey(c))
+                { 
+                    noRepeatDict.Remove(c);
+                    repeatDict.Add(c,true);
+                }
+                else
+                    noRepeatDict.Add(c, true);
+            }
+
+            foreach (var c in input)
+            {
+                if (noRepeatDict.ContainsKey(c)) return c;
+            }
+            return Char.MinValue;
+        }
+    }
+}
