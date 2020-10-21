770a3c7902464a9924d172929ef342060c760a22
Rodion <rodion.lim@hotmail.com>
2020-10-21 08:24:19 +0800

    Adding Binary Tree data structure

---
+++ csharp/Data Structures/BinaryTree/BinaryTree/CustomBinaryTree.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    class CustomBinaryTree
+    {
+        public Node root;
+        public class Node
+        {
+            public int? val;
+            public Node left;
+            public Node right;
+
+            public Node(int value)
+            {
+                val = value;
+            }
+            public Node() {}
+
+            public override string ToString()
+            {
+                return $"Node={val}";
+            }
+        }
+
+        public CustomBinaryTree() { }
+
+        public CustomBinaryTree(int val)
+        {
+            root = new Node(val);
+        }
+
+        public bool Find(int val)
+        {
+            var lastNode = GetLastPosition(val);
+            return (lastNode !=null) && (lastNode.val == val);
+        }
+
+        public void Insert(int val)
+        {
+            var lastNode = GetLastPosition(val);
+            var newNode = new Node(val);
+
+            if (lastNode == null) { root = newNode; return; }
+
+            if (lastNode.val == val)
+                return;
+            else
+            {
+                if (val > lastNode.val)
+                    lastNode.right = newNode;
+                else
+                    lastNode.left = newNode;
+            }
+        }
+
+        private Node GetLastPosition(int val)
+        {
+            Node current = root;
+            Node previous = root;
+
+            while (current != null)
+            {
+                if (val == current.val)
+
+                    return current;
+                else if (val > current.val)
+                {
+                    previous = current;
+                    current = current.right;
+                }
+                else if (val < current.val)
+                {
+                    previous = current;
+                    current = current.left;
+                }
+            }
+            return previous;
+        }
+
+        public void TraversePreOrder() => TraversePreOrder(root);
+
+        public void TraverseInOrder() => TraverseInOrder(root);
+
+        public void TraversePostOrder() => TraversePostOrder(root);
+
+        public int Height() => Height(root);
+
+        public int? Min() => Min(root);
+
+        public static bool Equals(CustomBinaryTree tree1, CustomBinaryTree tree2)
+        {
+            return Equals(tree1.root, tree2.root);
+        }
+
+        public bool IsBinarySearchTree()
+        {
+            return IsBinarySearchTree(root, int.MinValue, int.MaxValue);
+        }
+
+        public void PrintKDistanceNodes(int distance)
+        {
+            PrintKDistanceNodes(root, distance);
+        }
+
+        private void PrintKDistanceNodes(Node current, int distance)
+        {
+            if (current == null) return;
+            if (distance == 0)
+            {
+                Console.WriteLine(current.val);
+                return;
+            }
+            PrintKDistanceNodes(current.left, distance - 1); 
+            PrintKDistanceNodes(current.right, distance - 1);
+        }
+
+        private static bool Equals(Node node1, Node node2)
+        {
+            if (IsLeaf(node1) && IsLeaf(node2)) return node1.val == node2.val;
+            if ((!IsLeaf(node1) && IsLeaf(node2)) || (IsLeaf(node1) && !IsLeaf(node2))) return false;
+
+            var left = Equals(node1.left, node2.left);
+            var right = Equals(node1.right, node2.right);
+            return left && right;
+        }
+
+        private void TraversePreOrder(Node node)
+        {
+            if (node == null) return;
+            Console.WriteLine(node.val);
+            TraversePreOrder(node.left);
+            TraversePreOrder(node.right);
+        }
+
+        private void TraverseInOrder(Node node)
+        {
+            if (node == null) return;
+            TraverseInOrder(node.left);
+            Console.WriteLine(node.val);
+            TraverseInOrder(node.right);
+        }
+
+        private void TraversePostOrder(Node node)
+        {
+            if (node == null) return;
+            TraversePostOrder(node.left);
+            TraversePostOrder(node.right);
+            Console.WriteLine(node.val);
+        }
+
+        private int Height(Node node)
+        {
+            if (node == null) return -1;
+            if (IsLeaf(node)) return 0;
+            return 1 + Math.Max(Height(node.left), Height(node.right));
+        }
+
+        private int Min(Node node)
+        {
+            if (node == null) return 0;
+            if (IsLeaf(node)) return (int)node.val;
+            var left = Min(node.left);
+            var right = Min(node.right);
+            return Math.Min(Math.Min(left, right), (int)node.val);
+        }
+
+        private static bool IsLeaf(Node node)
+        {
+            return node.left == null && node.right == null;
+        }
+
+        private bool IsBinarySearchTree(Node node, int? lLimit, int? uLimit)
+        {
+            if (node == null) return true;
+
+            var withinRange = node.val > lLimit || node.val < uLimit;
+            if (!withinRange) return false;
+
+            var left = IsBinarySearchTree(node.left, lLimit, node.val);
+            var right = IsBinarySearchTree(node.right, node.val, uLimit);
+
+            return left && right;
+        }
+
+    }
+}
---
+++ csharp/Data Structures/BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var btree = new CustomBinaryTree();
+            Console.WriteLine(btree.Find(10));
+            btree.Insert(7);
+            btree.Insert(4);
+            btree.Insert(9);
+            btree.Insert(1);
+            btree.Insert(6);
+            btree.Insert(8);
+            btree.Insert(10);
+
+            var btree2 = new CustomBinaryTree();
+            btree2.Insert(7);
+            btree2.Insert(4);
+            btree2.Insert(9);
+            btree2.Insert(1);
+            btree2.Insert(6);
+            btree2.Insert(8);
+            btree2.Insert(10);
+
+            Console.WriteLine(btree.Find(10));
+
+            btree.TraversePreOrder();
+            Console.WriteLine();
+
+            btree.TraverseInOrder();
+            Console.WriteLine();
+
+            btree.TraversePostOrder();
+            Console.WriteLine();
+
+            Console.WriteLine($"Height: {btree.Height()}");
+            Console.WriteLine($"Min: {btree.Min()}");
+            Console.WriteLine($"Compare if 2 trees are equivalent: {CustomBinaryTree.Equals(btree, btree2)}");
+
+            Console.WriteLine($"Is binary search tree?: {btree.IsBinarySearchTree()}");
+            Console.WriteLine();
+
+            int k = 0;
+            Console.WriteLine($"Printing K Distance Nodes where k={k}");
+            btree.PrintKDistanceNodes(k);
+        }
+    }
+}
